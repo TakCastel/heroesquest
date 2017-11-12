b2c16300575ac374cb429ef25bf0a41cac6f46cc
TakCastel <takcastel@gmail.com>
2017-11-12 14:56:51 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Attributes from './character/Attributes';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Attributes/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/character/Skills.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Character.css';
+
+class Character extends Component {
+  render() {
+    return (
+      <div className="Character"></div>
+    );
+  }
+}
+
+export default Character;
